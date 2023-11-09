d8426fdd59495ff0cbedf0cc453281656a2708a8
Elostay <nastia60203@gmail.com>
2023-11-09 14:24:47 +0200

    add components

---
+++ src/components/Statistics.jsx
@@ -0,0 +1,18 @@
+const Statistics = ({ good, neutral, bad, total, positivePercentage }) => {
+  return (
+    <ul>
+      <li>Good: {good}</li>
+      <li>Neutral: {neutral}</li>
+      <li>Bad: {bad}</li>
+      <li>
+        Total:
+        {total(good, neutral, bad)}
+      </li>
+      <li>
+        Positive feedback:
+        {positivePercentage(good, neutral, bad)}%
+      </li>
+    </ul>
+  );
+};
+export default Statistics;
